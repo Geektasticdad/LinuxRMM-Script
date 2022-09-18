853e4da6c6499781a9144e40e939550f19b2bb2c
Geektasticdad <73676044+Geektasticdad@users.noreply.github.com>
2022-09-18 13:45:15 -0700

    Update rmmagent-linux.sh

--- rmmagent-linux.sh
+++ rmmagent-linux.sh
@@ -119,7 +119,7 @@ function go_install() {
         export GOPATH=/usr/local/go
         export GOCACHE=/root/.cache/go-build
 
-        echo "export PATH=/usr/local/go/bin" >> /root/.profile
+E        echo "export PATH=/usr/local/go/bin" >> /root/.profile
         echo "Golang Install Done !"
 }
 
